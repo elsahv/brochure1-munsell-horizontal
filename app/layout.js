d28495d4004666679b2b1610e910e745a5ca7b76
Elsa <elsahvy@gmail.com>
2023-05-10 15:08:53 -0700

    background fix

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="">
         <Header />
-        <main className="bg-munsell h-[750px] mt-[50px]">{children}</main>
+        <main className="h-[750px] mt-[50px]">{children}</main>
         <Footer />
       </body>
     </html>
