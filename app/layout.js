670a4f9badbbd631952c5b9b28bd58a03e1a610e
Elsa <elsahvy@gmail.com>
2023-08-10 14:44:56 -0700

    vercel test

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Elsa Hovey | Portfolio",
