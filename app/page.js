b4b01bd9d7cf2c367e52e829bace8b41ebf3ca1c
Elsa <elsahvy@gmail.com>
2023-08-17 12:57:25 -0700

    rm lorem

--- app/page.js
+++ app/page.js
@@ -18,10 +18,11 @@ export default function Home() {
         </div>
         <p className="text-aquamarine mt-1 mb-5 mx-1 text-2xl pt-2 pl-1">
           {"I'm"} a web developer based in Joshua Tree, CA. Listed below are
-          some websites {"I've"} created: Lorem, ipsum dolor sit amet
+          some websites {"I've"} created:{" "}
+          {/*Lorem, ipsum dolor sit amet
           consectetur adipisicing elit. Vero aperiam quam, consequuntur unde
           doloribus earum eius ratione ab non, laboriosam possimus dolore, quo
-          perspiciatis. Quae modi in quos iste nihil.
+          perspiciatis. Quae modi in quos iste nihil. */}
           {/* I create niche websites and solutions for small business owners and creatives. */}
         </p>
         <Websites />
